d3708bd5ac8e455204ed7facf96e607dc3168985
José Robles Bastidas <43571325+Jose-Robles@users.noreply.github.com>
2018-09-30 19:47:49 +0200

    Update Ejercicio09.java

--- Tema 1/Ejercicio09.java
+++ Tema 1/Ejercicio09.java
@@ -1,15 +1,22 @@
 /**
- * Ejercicio 09
+ * Ejercicio 09 tema 01
  * 
  * 
  * @author José Robles Bastidas
  */
 public class Ejercicio09 {
     public static void main(String[] args) {
-        System.out.println("    *    ");
-        System.out.println("   * *   ");
-        System.out.println("  *   *  ");
-        System.out.println(" *     * ");
-        System.out.println("*********");
+        System.out.println("  |-----------*-----------|");
+        System.out.println("  |  --------*·*--------  |");
+        System.out.println("  |    -----*···*-----    |");
+        System.out.println("  |      --*·····*--      |");
+        System.out.println("  |       *********       |");        
+        System.out.println("  |      * ------- *      |");        
+        System.out.println("  |     *    ---    *     |");
+        System.out.println("  |    *      -      *    |");
+        System.out.println("  |   *     *   *     *   |");
+        System.out.println("  |  *   *         *   *  |");
+        System.out.println("  | *  *             *  * |");
+        System.out.println("  |*                     *|");
     }
 }
